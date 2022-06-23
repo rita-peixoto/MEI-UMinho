a13606954752b9227237ec5e98428c0378a136cf
Rita Peixoto <49651590+rita-peixoto@users.noreply.github.com>
2022-06-23 17:11:01 +0100

    Delete gke-wikijs directory

--- gke-wikijs/startup.template.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Set the gcloud project name
-gcloud config set project ${GCP_PROJECT_NAME}
-
-# Activate the gcloud service account
-gcloud auth activate-service-account --key-file=${SERVICE_ACCOUNT_PRIVATE_KEY_JSON}
